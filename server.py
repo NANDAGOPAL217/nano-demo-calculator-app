706d307d9c8d8d591bd7e7960cfddbef619005df
NANDAGOPAL217 <gopalnanda217@gmail.com>
2023-08-16 15:10:21 +0530

    nanada_test_final

--- server.py
+++ server.py
@@ -1,24 +1,60 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+employees = [] 
 
-@app.route("/calculator/greeting", methods=['GET'])
+# Greeting
+@app.route("/greeting", methods=['GET'])
 def greeting():
     return 'Hello world!'
 
-@app.route("/calculator/add", methods=['POST'])
-def add():
-    number = request.json
-    result = number['first'] + number['second']
-    return {"result": result }
+# Create Employee
+@app.route('/employee', methods=['POST'])
+def create_employee():
+    try:
+        data = request.json
+        if 'name' in data and 'city' in data:
+            employee_id = str(len(employees) + 1)
+            data['employeeId'] = employee_id
+            employees.append(data)
+            return jsonify({"employeeId": employee_id}), 201
+        else:
+            return jsonify({"message": "Invalid data"}), 400
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
-@app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    numbers = request.json
-    result = numbers['first'] - numbers['second']
-    return {"result": result}
+# Get all Employee details
+@app.route('/employees/all', methods=['GET'])
+def get_all_employees():
+    return jsonify(employees)
+
+# Get Employee details
+@app.route('/employee/<employee_id>', methods=['GET'])
+def get_employee(employee_id):
+    employee = next((e for e in employees if e['employeeId'] == employee_id), None)
+    if employee:
+        return jsonify(employee)
+    return jsonify({"message": f"Employee with {employee_id} was not found"}), 404
+
+# Update Employee
+@app.route('/employee/<employee_id>', methods=['PUT'])
+def update_employee(employee_id):
+    data = request.json
+    employee = next((e for e in employees if e['employeeId'] == employee_id), None)
+    if employee:
+        employee.update(data)
+        return jsonify(employee), 201
+    return jsonify({"message": f"Employee with {employee_id} was not found"}), 404
+
+# Delete Employee
+@app.route('/employee/<employee_id>', methods=['DELETE'])
+def delete_employee(employee_id):
+    employee = next((e for e in employees if e['employeeId'] == employee_id), None)
+    if employee:
+        employees.remove(employee)
+        return jsonify(employee), 200
+    return jsonify({"message": f"Employee with {employee_id} was not found"}), 404
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
